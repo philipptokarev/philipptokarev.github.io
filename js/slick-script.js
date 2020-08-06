19221665f28cc9f26cdd1bd52f3f8fcd95434b28
Phil <tokarev.ph@gmail.com>
2020-08-06 14:19:57 +0300

    x

--- js/slick-script.js
+++ js/slick-script.js
@@ -1,12 +1,14 @@
 var $status = $('.pagingInfo');
 var $amout = $('.amount');
+var $lstatus = $('.letter-pagingInfo');
+var $lamout = $('.letter-amount');
+var $slickLetters = $('#letters-slider');
 var $slickElement = $('#slider');
 var $slickProducts = $('#products-slider');
 var $slickTeam = $('#about-team-slider');
 var $bg_image = $('.background-slider');
 let images = ["https://i.pinimg.com/originals/5d/26/49/5d2649e7901eb0c006b338c2b1634e93.jpg", "https://steelcase-res.cloudinary.com/image/upload/c_fill,dpr_auto,q_70,h_656,w_1166/v1571816355/www.steelcase.com/eu-en/2019/10/23/19-0129615.png", "https://wall-murals.eu/image/cache/data/0305-fototapeti/200/0305-200P2-WEB-fototapeti-chervena-roza-mural-red-rose-P2-1200x800_0.jpg"];
 var city = "Выбор";
-
 $slickElement.on('init reInit afterChange', function (event, slick, currentSlide, nextSlide) {
   var i = (currentSlide ? currentSlide : 0) + 1;
   $status.text(i);
@@ -14,11 +16,24 @@ $slickElement.on('init reInit afterChange', function (event, slick, currentSlide
   $bg_image.attr('src', images[i-1]);
 });
 
+$slickLetters.on('init reInit afterChange', function (event, slick, currentSlide, nextSlide) {
+  var i = (currentSlide ? currentSlide : 0) + 1;
+  $lstatus.text(i);
+  $lamout.text('/ ' + slick.slideCount);
+});
+
 $slickElement.slick({
   slidesToShow: 1,
   slidesToScroll: 1,
   prevArrow: $('.arrow-left'),
   nextArrow: $('.arrow-right')
+});
+
+$slickLetters.slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  prevArrow: $('.letter-arrow-left'),
+  nextArrow: $('.letter-arrow-right')
 });
 $slickProducts.slick({
   slidesToShow: 1,
