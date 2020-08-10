da3f8f67382feb87df4bbea12e60d217001d388b
Phil <tokarev.ph@gmail.com>
2020-08-10 13:34:01 +0300

    c

--- js/script.js
+++ js/script.js
@@ -12,6 +12,11 @@ function menuClick(){
 function footerMenuShow(el){
   $(el).parent().find("ul").toggle();
 }
+
+function catalogMenuShow(el){
+  $(el).parent().find(".filter-item").toggle();
+}
+
 function cityClick(){
   var c = $(".city b.name").text();
   $(".city b.name").text(city);
@@ -30,3 +35,47 @@ function chooseCity(el){
 function callForm(){
   $(".callback-form").toggleClass("show");
 }
+
+$(".polzunok-5").slider({
+  min: 0,
+  max: 7000000,
+  values: [2000000, 5000000],
+  range: true,
+  animate: "fast",
+  step: 1000,
+  slide : function(event, ui) {
+  $(".polzunok-input-5-left").val(ui.values[ 0 ]);
+  $(".polzunok-input-5-right").val(ui.values[ 1 ]);
+  }
+});
+$(".polzunok-input-5-left").val($(".polzunok-5").slider("values", 0));
+$(".polzunok-input-5-right").val($(".polzunok-5").slider("values", 1));
+$(document).focusout(function() {
+  var input_left = $(".polzunok-input-5-left").val().replace(/[^0-9]/g, ''),
+  opt_left = $(".polzunok-5").slider("option", "min"),
+  where_right = $(".polzunok-5").slider("values", 1),
+  input_right = $(".polzunok-input-5-right").val().replace(/[^0-9]/g, ''),
+  opt_right = $(".polzunok-5").slider("option", "max"),
+  where_left = $(".polzunok-5").slider("values", 0);
+  if (input_left > where_right) {
+    input_left = where_right;
+  }
+  if (input_left < opt_left) {
+    input_left = opt_left;
+  }
+  if (input_left == "") {
+  input_left = 0;
+  }
+  if (input_right < where_left) {
+    input_right = where_left;
+  }
+  if (input_right > opt_right) {
+    input_right = opt_right;
+  }
+  if (input_right == "") {
+  input_right = 0;
+  }
+  $(".polzunok-input-5-left").val(input_left);
+  $(".polzunok-input-5-right").val(input_right);
+  $(".polzunok-5").slider( "values", [ input_left, input_right ] );
+});
--- js/slick-script.js
+++ js/slick-script.js
@@ -5,6 +5,7 @@ var $lamout = $('.letter-amount');
 var $slickLetters = $('#letters-slider');
 var $slickElement = $('#slider');
 var $slickProducts = $('#products-slider');
+var $slickPortfolio = $('#portfolio-slider');
 var $slickTeam = $('#about-team-slider');
 var $bg_image = $('.background-slider');
 let images = ["https://i.pinimg.com/originals/5d/26/49/5d2649e7901eb0c006b338c2b1634e93.jpg", "https://steelcase-res.cloudinary.com/image/upload/c_fill,dpr_auto,q_70,h_656,w_1166/v1571816355/www.steelcase.com/eu-en/2019/10/23/19-0129615.png", "https://wall-murals.eu/image/cache/data/0305-fototapeti/200/0305-200P2-WEB-fototapeti-chervena-roza-mural-red-rose-P2-1200x800_0.jpg"];
@@ -40,6 +41,12 @@ $slickProducts.slick({
   slidesToScroll: 1,
   prevArrow: $('.product-arrow-left'),
   nextArrow: $('.product-arrow-right')
+});
+$slickPortfolio.slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  prevArrow: $('.arrow-left'),
+  nextArrow: $('.arrow-right')
 });
 $slickTeam.slick({
   slidesToShow: 1,
