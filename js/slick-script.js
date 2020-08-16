00f04ceaa6bf3b3d4ac755ad1ac4b961349f6e1f
Phil <tokarev.ph@gmail.com>
2020-08-16 18:39:17 +0300

    X

--- js/slick-script.js
+++ js/slick-script.js
@@ -7,6 +7,7 @@ var $slickElement = $('#slider');
 var $slickProducts = $('#products-slider');
 var $slickPortfolio = $('#portfolio-slider');
 var $slickTeam = $('#about-team-slider');
+var $slickBenefits = $('.benefits');
 var $bg_image = $('.background-slider');
 let images = ["https://i.pinimg.com/originals/5d/26/49/5d2649e7901eb0c006b338c2b1634e93.jpg", "https://steelcase-res.cloudinary.com/image/upload/c_fill,dpr_auto,q_70,h_656,w_1166/v1571816355/www.steelcase.com/eu-en/2019/10/23/19-0129615.png", "https://wall-murals.eu/image/cache/data/0305-fototapeti/200/0305-200P2-WEB-fototapeti-chervena-roza-mural-red-rose-P2-1200x800_0.jpg"];
 var city = "Выбор";
@@ -54,3 +55,15 @@ $slickTeam.slick({
   prevArrow: $('.team-arrow-left'),
   nextArrow: $('.team-arrow-right')
 });
+
+if($(window).width() <= 1100)
+  $slickBenefits.slick({
+    responsive: [{
+      breakpoint: 1100,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true
+      }
+    }]
+  });
