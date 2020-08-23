960d4e4492c9334b64bedbb6fd1503674498cfa0
Phil <tokarev.ph@gmail.com>
2020-08-24 00:06:01 +0300

    x

--- js/map.js
+++ js/map.js
@@ -1,15 +1,15 @@
 ymaps.ready(function () {
-  var myMap = new ymaps.Map('map', {
-    center: [55.818847, 49.114732],
-    zoom: 15,
-    controls: ['zoomControl', 'searchControl', 'typeSelector',  'fullscreenControl', 'routeButtonControl']
-  }),
-
-      myPlacemark = new ymaps.Placemark([55.818847, 49.114732], {
-        hintContent: 'Станнум'
-      });
-  myMap.geoObjects.add(myPlacemark);
-  myMap.controls
-  // Кнопка изменения масштаба.
-    .add('zoomControl', { left: 5, top: 5 })
+  var address = "г. Казань, ул. Чистопольская д. 22"
+  var hint = ymaps.geocode(address, {results: 1}).then(function (res) {
+    var object = res.geoObjects.get(0),
+    coords = object.geometry.getCoordinates();
+    var myMap = new ymaps.Map('map', {
+      center: coords,
+      zoom: 15,
+      controls: ['zoomControl', 'searchControl', 'typeSelector',  'fullscreenControl', 'routeButtonControl']
+    });
+    myPlacemark = new ymaps.Placemark(coords, { hintContent: 'Станнум' });
+    myMap.geoObjects.add(myPlacemark);
+    myMap.controls.add('zoomControl', { left: 5, top: 5 })
+  });
 });
